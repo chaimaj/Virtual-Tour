2ac672ea47e7d3827db37fcbfa742fa8511eea6f
chaimaj <chaima.jemmali@gmail.com>
2020-11-05 09:58:00 -0500

    project commit

---
+++ projet2/js/composante4.js
@@ -0,0 +1,45 @@
+
+var carthage = { 'panorama': [
+  { //premiere image
+    'mapFile': 'textures/image4.jpg',
+    'navMarkers': [
+      { 'target': 1,
+        'x': -99346.687712829447,
+        'y': -9994.363267749186887,
+        'z': -99360.2618376509449
+      }
+    ],
+    'markers': [
+      
+      { 'archeo': 'basilique',
+        x: 494.99702534411864,
+        y: -45.26309611002569,
+        z: -166.80808062147547
+      },
+     
+     
+    ]
+    
+  }, 
+  ],
+
+  
+  
+  
+  
+  
+  //debut donneearcheo
+  'donneearcheo':{
+    
+    'basilique': {
+      'name': 'info',
+      'information': 'Bassin la Schola',
+      'image': 'img/info4.jpg'
+    },
+	  
+
+	
+
+  } 
+
+};
